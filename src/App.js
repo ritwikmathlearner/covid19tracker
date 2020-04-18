3e47078c2c10132535868e363bd7e3fc0c5475dd
ritwikmathlearner <ritwikmathhithub@gmail.com>
2020-04-18 13:12:07 +0530

    Alpha version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards, Chart, CountryPicker} from './components';
+import {Typography, Box} from '@material-ui/core';
+import styles from './App.module.css';
+import {fetchData} from './api';
+import {fetchDailyData} from './api';
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+    }
+
+    handleCountryChange = async (country)=>{
+        const fetchedData = await fetchData(country);
+        this.setState({data: fetchedData, country: country});
+    }
+
+    render() {
+        const {data, country} = this.state;
+        return (
+            <div className={styles.container}>
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
+                <Box p={5}>
+                    <Typography color="textSecondary" variant="body2" align="center">&copy; Developed and Maintained by Ritwik Math</Typography>
+                </Box>
+            </div>
+        )
+    }
 }
 
 export default App;
